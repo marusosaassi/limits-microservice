21ade6c7c21e13788ae6104d899e6760b812e950
Maru Sosa <ms@wayakna.com>
2022-09-13 11:35:25 -0500

    hard coded limits service

---
+++ src/main/java/com/example/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,13 @@
+package com.example.microservices.limitsservice.controller;
+
+import com.example.microservices.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(1,1000);
+    }
+}
